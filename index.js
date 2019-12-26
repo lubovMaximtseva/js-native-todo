115b066e5cb76f11f089cfec20aa61fc58bcec0e
lubovMaximtseva <lubov.maximtseva@gmail.com>
2019-12-26 15:18:44 +0300

    added id for strong tag, fixed function output

--- index.js
+++ index.js
@@ -95,6 +95,6 @@ function toggleTask(event) {
 
 function countActiveTasks(tasks) {
   const newTasksList = tasks.filter(task => task.completed === false);
-  const strong = document.querySelector("strong");
-  strong.innerHTML = `${newTasksList.length}`;
+  const strong = document.getElementById("countTask");
+  strong.innerHTML = newTasksList.length;
 }
