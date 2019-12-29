74b7900ec75f9715dc8deda710fdaf010b35ba47
lubovMaximtseva <lubov.maximtseva@gmail.com>
2019-12-29 20:41:07 +0300

    save filter aftr reload

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ window.onload = () => {
   checkFooter(tasksList);
   renderTasks(tasksList);
   countActiveTasks(tasksList);
+  const filter = window.location.hash.slice(2);
+  filtrTasks(filter);
 };
 
 function createListItem(task) {
